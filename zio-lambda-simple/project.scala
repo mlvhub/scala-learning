daaae5d2fe743a11e49ffbdc8cdbca98bbb4fa89
Miguel Lopez V <miguellova@gmail.com>
2024-04-11 00:07:20 +0100

    First version of Lambda, just using ZIOApp, not working

---
+++ zio-lambda-simple/project.scala
@@ -0,0 +1,4 @@
+//> using scala "3"
+//> using dep "dev.zio::zio::2.0.21"
+//> using dep "dev.zio::zio-lambda::1.0.4"
+//> using dep "dev.zio::zio-lambda-event::1.0.4"
---
+++ zio-lambda-simple/src/ziolambdasimple/LambdaHandler.scala
@@ -0,0 +1,20 @@
+package ziolambdasimple
+
+import zio.Console._
+import zio._
+import zio.lambda._
+import zio.lambda.event._
+import java.io.IOException
+
+object LambdaHandler extends ZIOAppDefault {
+
+  def app(
+      event: String,
+      context: Context
+  ): ZIO[Any, IOException, String] = for {
+    _ <- printLine("Hello, world!")
+  } yield "Handler ran successfully"
+
+  override val run =
+    ZLambdaRunner.serve(app)
+}
